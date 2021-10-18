9a5d2ebcc404296ffe1df6f08657ad84cf233592
Meilani Agistia04 <92595264+MeilaniAgistia@users.noreply.github.com>
2021-10-18 05:38:11 -0700

    Add files via upload

---
+++ F1G121071_Meilani Agistia_Menghitung Luas Lingkaran.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+ {
+   std:cout<<"Meilani Agistia \n";
+   float luas, phi= 4.5;
+   int r;
+   
+   cout<<"masukan nilai jari-jari : " ;
+   cin>>r;
+   cout<<"luas lingkaran : " ;
+   cin>>luas;
+     
+     luas = phi * r * r;
+ return 0;
+   
+}   
+   
+   
+   
+   
+   
+   
+ 
+  
+  
+  
+
+  
+
+    
+    
+    
+    
+  
+    
