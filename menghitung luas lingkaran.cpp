80180ecdc92bfae900b05c84942121ad944b84bc
Meilani Agistia04 <92595264+MeilaniAgistia@users.noreply.github.com>
2021-10-18 05:10:06 -0700

    Meilani Agistia_F1G121071

---
+++ menghitung luas lingkaran.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+ {
+   std:cout<<"Meilani Agistia \n";
+   float luas, a= 4.5;
+         luas, b= 3.2;
+         luas, c= 2.1;
+   int r;
+   
+   cout<<"masukan nilai jari-jari : " ;
+   cin>>r;
+   cout<<"luas lingkaran : " ;
+   cin>>luas;
+     
+     luas = a * r * r;
+   
+ return 0;
+   
+   
+   
+   
+   
+   
+   
+   
+ } 
+  
+  
+  
+
+  
+
+    
+    
+    
+    
+  
+    
