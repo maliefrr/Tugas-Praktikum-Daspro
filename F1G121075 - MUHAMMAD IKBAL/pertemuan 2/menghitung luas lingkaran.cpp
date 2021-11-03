f59701bd49485c91525ab6e02b39f01d68f62068
Lynx-riel <adventerous84@gmail.com>
2021-11-03 09:22:18 +0800

    Menambahkan file tugas 3

--- F1G121075 - MUHAMMAD IKBAL/pertemuan 2/menghitung luas lingkaran.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-	float jari_jari;
-	cout<<"Masukan panjang jari jari lingkaran : ";
-	cin>>jari_jari;
-	cout<<"Luas lingkaran = "<<3.14*jari_jari*jari_jari;
-}
