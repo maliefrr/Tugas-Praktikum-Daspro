28e9cf4daa0554eb0ded7712ee4f9c4354832259
Fajarrahmi666 <muhammadrahmifajar69@gmail.com>
2021-11-07 01:48:39 -0700

    menambahakan tugas pertemuan 5

---
+++ F1G121078- MUHAMMAD RAHMI FAJAR/PERTEMUAN 5/TUGAS DASPRO 5 FOR LOOP.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int a,b,c,hasil;
+		
+		cout <<"masukan input nilai a =" ;
+		cin >>a;
+		cout <<"masukan input nilai b =";
+		cin >>b;
+		cout <<"masukan input nilai c =";
+		cin >>c;
+		
+		hasil = a + b + c;
+		
+		cout <<" hasil penjumlahn dari inputan nilai tersebut adalah = "<<hasil<<endl;
+			
+		
+			cout << "for loop"<<endl;
+				for(int i = hasil; i <= 100; i++){
+					cout << i <<endl;	
+				}
+				
+
+
+
+
+}
