3d2197777396889a7725027f121d332a232e234c
Agung Rezky <92588511+agungrezky06@users.noreply.github.com>
2021-11-10 08:37:14 +0800

    Menambahkan tugas pertemuan 5

---
+++ F1G121015 - Agung Rezky/Pertemuan 5/Program do while.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	system("color 6");
+	
+	a:
+	int a = 1, input, panjang, harga = 2000, hasil = 0;
+	cout<<"====== Welcome to Toko Kain Agung jaya prima ======" << endl;
+	cout<<"\n Harga kain Permeter = Rp2000" << endl;
+	//Harga kain rata Rp.2000 untuk semua jenis kain dengan warna yang berbeda-beda
+	cout<<"Berapa kali ingin melakukan pembeli kain berbeda : ";
+	//memasukkan angka akan melakukan perulangan sebanyak apa yang anda inputkan
+	cin>>input;
+
+	do {
+		cout<<"masukan panjang kain ke "<<a<<"(m): ";
+		cin>>panjang;
+		//inputkan angka (panjang kain) yang akan anda pesan
+		a++;
+		hasil=hasil+panjang;
+		//berikut akan tampil total panjang kain yang anda pesan
+	}
+	while (a <= input);
+
+	cout<<"Jumlah panjang kain yang anda pesan adalah = "<<hasil<<" m"<<endl;
+	cout<<"Total harga yang anda bayar adalah = Rp"<<hasil*harga;
+	//Harga yang akan anda bayar akan muncul dilayar dengan mengalikan total panjang kain yang anda pesan dengan harga permeter kain
+	cout<<endl<<endl;
+	goto a;
+	return 0;
+}
