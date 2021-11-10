2de83cfca3c1f7d0623b3f37dc617ef47ab3a184
idafadhillah <fadhillahida@gmail.com>
2021-11-10 23:00:32 +0700

    Menambahkan tugas pertemuan 5

---
+++ F1G121021 - Ida Fadhillah As'adiyah/Pertemuan 5/PengulanganWhile_Ida Fadhillah As'adiyah(F1G121021).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int n , a, total;
+	int i=1;
+	char ulang;
+	
+	cout<<"-------------------------------------------------------"<<endl;
+	cout<<"|                Program Pengulangan                  |"<<endl;
+	cout<<"-------------------------------------------------------"<<endl;
+	cout<<"|          Nama    : Ida Fadhillah As'adiyah          |"<<endl;
+	cout<<"|          NIM     : F1G121021                        |"<<endl;
+	cout<<"|          Jurusan : Ilmu Komputer                    |"<<endl;
+	cout<<"-------------------------------------------------------"<<endl;
+	
+	cout<<endl;
+	coba_lagi :
+	cout<<"Berapa banyak pengulangan yang ingin di input? : "<<endl;
+	cin>>n;
+	total=0;
+	
+	cout<<endl;
+	
+	while(i<=n){
+		cout<<"Bilangan ke-"<<i<<" : ";
+		cin>>a;
+		total+=a;
+		i++;
+	}
+	
+	system("cls");
+	cout<<endl<<"Jumlah dari "<<n<<" bilangan yang Anda masukkan = "<<total<<endl;
+	
+	
+	//Big Thank's buat Kak Alief yang sudah berbagi ilmu sampai pertemuan ke-5. See you next time Kak! \(0_0)/
+	return 0;
+	
+}
