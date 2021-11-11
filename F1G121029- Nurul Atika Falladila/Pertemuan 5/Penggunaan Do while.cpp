78ca896b0da049c31b57aad7d7b7ae794da303dd
Nurul Atika Falladila <92599986+NurulAtika@users.noreply.github.com>
2021-11-11 23:23:27 +0800

    Menambahkan Tugas Pertemuan 5

---
+++ F1G121029- Nurul Atika Falladila/Pertemuan 5/Penggunaan Do while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+	cout<<"=========================\n";
+	cout<<"      do while loop      \n";
+	cout<<"=========================\n";
+	int i = 1;
+	do {
+		cout << i << " ";
+		++i;
+	}
+	while (i <=5);
+	 
+	 return 0;
+}
---
+++ F1G121029- Nurul Atika Falladila/Pertemuan 5/Penggunaan While.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+cout<<"=============================\n";
+cout<<"          while loop         \n";
+cout<<"=============================\n";
+    int i = 1;
+    while (i <=5){
+    	cout<< i << " ";
+    	++i;
+	}
+}
+	
